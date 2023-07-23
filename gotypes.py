42ace2ec2b726b77585013a4aee94f9a5161be37
Bartosz Wójcik <bartoszmwojcik@gmail.com>
2023-07-23 20:56:38 +0200

    First commit.

---
+++ gotypes.py
@@ -0,0 +1,87 @@
+# Filename: gotypes.py
+
+from enum import Enum
+from collections import namedtuple
+
+class Color(Enum):
+    BLACK = 1
+    WHITE = 2
+
+class Player:
+    def __init__(self, color):
+        self.color = color
+
+    @property
+    def other(self):
+        return Player(Color.BLACK) if self.color == Color.WHITE else Player(Color.WHITE)
+
+    def __str__(self):
+        return f'Player(\'{self.color.name})'
+    
+    def __eq__(self, other):
+        return isinstance(other, Player) and \
+            self.color == other.color
+
+class Point (namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1)
+    ]
+    
+    def __deepcopy__(self, memodict={}):
+        # These are very immutable.
+        return self
+
+class Move:
+    def __init__(self, point=None, is_pass=False, is_resign=False):
+        assert (point is not None) ^ is_pass ^ is_resign
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
+
+    @classmethod
+    def play(cls, point):
+        return Move(point=point)
+
+    @classmethod
+    def pass_turn(cls):
+        return Move(is_pass=True)
+
+    @classmethod
+    def resign(cls):
+        return Move(is_resign=True)
+
+
+class GoString():
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones)
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
